7473851b30f5bab30ed850f6e36d622133d74d99
timur-rapid <99427891+timur-rapid@users.noreply.github.com>
2022-03-04 06:53:18 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -25,7 +25,7 @@ apt-get update
 apt-get -y install nginx
 
 # install essential package
-apt-get -y install nano iptables dnsutils fail2ban openvpn screen whois ngrep unzip unrar
+apt-get -y install nano iptables dnsutils openvpn screen whois ngrep unzip unrar
 # install neofetch
 apt-get -y install neofetch
 echo "clear" >> .bash_profile
@@ -101,6 +101,34 @@ apt-key add jcameron-key.asc
 apt-get update && apt-get -y install webmin
 sed -i -e 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
 service webmin restart
+
+# Instal DDOS Flate
+if [ -d '/usr/local/ddos' ]; then
+	echo; echo; echo "Please un-install the previous version first"
+	exit 0
+else
+	mkdir /usr/local/ddos
+fi
+clear
+echo; echo 'Installing DOS-Deflate 0.6'; echo
+echo; echo -n 'Downloading source files...'
+wget -q -O /usr/local/ddos/ddos.conf http://www.inetbase.com/scripts/ddos/ddos.conf
+echo -n '.'
+wget -q -O /usr/local/ddos/LICENSE http://www.inetbase.com/scripts/ddos/LICENSE
+echo -n '.'
+wget -q -O /usr/local/ddos/ignore.ip.list http://www.inetbase.com/scripts/ddos/ignore.ip.list
+echo -n '.'
+wget -q -O /usr/local/ddos/ddos.sh http://www.inetbase.com/scripts/ddos/ddos.sh
+chmod 0755 /usr/local/ddos/ddos.sh
+cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
+echo '...done'
+echo; echo -n 'Creating cron to run script every minute.....(Default setting)'
+/usr/local/ddos/ddos.sh --cron > /dev/null 2>&1
+echo '.....done'
+echo; echo 'Installation has completed.'
+echo 'Config file is at /usr/local/ddos/ddos.conf'
+echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
+
 #download
 cd /usr/bin
 wget -O menu "https://raw.githubusercontent.com/Natch0141/ubuntu/master/menu.sh"
